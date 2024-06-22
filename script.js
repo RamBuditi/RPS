dff2d82c73c038a5b5c6442d3ef555f13da074ae
Ram Buditi <rambuditi@gmail.com>
2024-06-22 07:13:44 -0400

    finished the computer and human choice functions

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 console.log("hello world");
+function getComputerChoice()
+{
+    num = Math.floor(Math.random() * 3);
+    if(num == 0)
+        {
+            return "rock";
+        }
+    else if(num == 1)
+        {
+            return "paper";
+        }
+    else
+    {
+        return "scissor";
+    }
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice()
+{
+  let ans;
+
+while(ans != "rock" || ans != "paper" || ans != "scissor")
+    {
+     ans = prompt("rock, paper, or scissor?");
+    if(ans == "rock" || ans == "paper" || ans == "scissor")
+        {
+            return ans;
+        }
+    else
+    {
+        console.log("Invald input please try again case sensitive");
+    }
+}
+}
+
+console.log(getHumanChoice());
